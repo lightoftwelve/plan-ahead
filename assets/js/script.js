3572e18320fb116752cced07e34990067605dee9
lightoftwelve <info@lightoftwelve.com>
2023-07-01 17:14:41 -0400

    added past, present, future classes to scheduler that dynamically modifies to the current time

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,26 +2,29 @@ $(document).ready(function () {
   var today = dayjs();
   $('#currentDay').text(today.format('dddd MMM D, YYYY'));
 
-  $(function () {
-    var timeBlocksContainer = $('#timeBlocks');
+  var timeBlocksContainer = $('#timeBlocks');
+  var workHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+  var currentHour = dayjs().hour();
 
-    var workHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+  workHours.forEach(function (hour) {
+    var timeBlock = $('<div>').addClass('row time-block').attr('data-hour', hour);
+    var timeLabel = $('<div>').addClass('col-2 col-md-1 hour text-center py-3');
+    timeLabel.text(hour <= 12 ? hour + 'AM' : (hour - 12) + 'PM');
+    var entrySpace = $('<textArea>').addClass('col-8 col-md-10 description').attr('rows', '3');
+    var saveBtn = $('<button>').addClass('btn saveBtn col-2 col-md-1').attr('aria-label', 'save');
+    var saveIcon = $('<i>').addClass('fas fa-save').attr('aria-hidden', 'true');
+    saveBtn.append(saveIcon);
 
-    workHours.forEach(function (hour) {
-      var timeBlock = $('<div>').addClass('row time-block').attr('data-hour', hour);
+    timeBlock.append(timeLabel, entrySpace, saveBtn);
+    timeBlocksContainer.append(timeBlock);
 
-      var timeLabel = $('<div>').addClass('col-2 col-md-1 hour text-center py-3');
-      timeLabel.text(hour <= 12 ? hour + 'AM' : (hour - 12) + 'PM');
-
-      var entrySpace = $('<textArea>').addClass('col-8 col-md-10 description').attr('rows', '3');
-
-      var saveBtn = $('<button>').addClass('btn saveBtn col-2 col-md-1').attr('aria-label', 'save');
-      var saveIcon = $('<i>').addClass('fas fa-save').attr('aria-hidden', 'true');
-      saveBtn.append(saveIcon);
-
-      timeBlock.append(timeLabel, entrySpace, saveBtn);
-      timeBlocksContainer.append(timeBlock);
-    });
+    if (hour === currentHour) {
+      timeBlock.addClass('present');
+    } else if (hour > currentHour) {
+      timeBlock.addClass('future');
+    } else {
+      timeBlock.addClass('past');
+    }
   });
 });
 
